a2b1594a02024d09b742920ef72d7e4c862973e9
Karan <email@karanshergill.com>
2024-07-27 11:55:48 +0530

    modify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Dark-Cyan': 'hsl(185, 75%, 39%)',
+        'Primary-Very-Dark-Desaturated-Blue': 'hsl(229, 23%, 23%)',
+        'Primary-Dark-Grayish-Blue': 'hsl(227, 10%, 46%)',
+        'Neutral-Dark-Gray': 'hsl(0, 0%, 59%)'
+      },
+      fontFamily: {
+        'Kumbh': ['Kumbh Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
